55a2c01decfe899b0088f678788c5d3e0361e748
liuqianhong <liuqianhong@outlook.com>
2024-12-04 09:39:56 +0800

    增加png相机品牌

--- camera_watermark_node.py
+++ camera_watermark_node.py
@@ -6,50 +6,53 @@ from .base_node import BaseNode
 
 class CameraWatermarkNode(BaseNode):
     @classmethod
-    def read_svg_files(cls):
-        svg_data_uris = {}
+    def read_brand_files(cls):
+        brand_data_uris = {}
         brand_options = []
         brand_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "brand")
         
         if os.path.exists(brand_dir):
             for file in os.listdir(brand_dir):
-                if file.endswith('.svg'):
+                if file.endswith(('.svg', '.png')):
                     brand_name = os.path.splitext(file)[0]
                     brand_options.append(brand_name)
-                    svg_path = os.path.join(brand_dir, file)
-                    with open(svg_path, 'rb') as f:
-                        svg_content = f.read()
-                        svg_base64 = base64.b64encode(svg_content).decode('utf-8')
-                        svg_data_uris[brand_name] = f"data:image/svg+xml;base64,{svg_base64}"
+                    brand_file_path = os.path.join(brand_dir, file)
+                    with open(brand_file_path, 'rb') as f:
+                        brand_file_content = f.read()
+                        brand_file_base64 = base64.b64encode(brand_file_content).decode('utf-8')
+                        if file.endswith('.svg'):
+                            brand_data_uris[brand_name] = f"data:image/svg+xml;base64,{brand_file_base64}"
+                        elif file.endswith('.png'):
+                            brand_data_uris[brand_name] = f"data:image/png;base64,{brand_file_base64}"
         
         if not brand_options:
-            brand_options = ["leica"]
+            brand_options = ["liuqianhong"]
             
-        return brand_options, svg_data_uris
+        return brand_options, brand_data_uris
 
     def __init__(self):
         super().__init__()
         template_dir = os.path.dirname(os.path.realpath(__file__))
         self.jinja_env = Environment(loader=FileSystemLoader(template_dir))
-        _, self.svg_data_uris = self.read_svg_files()
+        _, self.brand_data_uris = self.read_brand_files()
 
     @classmethod
     def INPUT_TYPES(cls):
-        brand_options, _ = cls.read_svg_files()
         current_time = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+        brand_options, _ = cls.read_brand_files()
 
         return {
             "required": {
-                "model": ("STRING", {"default": "COMFYUI X1 ULTRA"}),
+                "model": ("STRING", {"default": "COMFYUI ULTRA"}),
                 "date": ("STRING", {"default": current_time}),
-                "brand": (brand_options, {"default": brand_options[0]}),
+                "brand": (brand_options, {"default": "liuqianhong"}),
                 "device": ("STRING", {"default": "23mm f/1.0 1/320 ISO1495"}),
                 "gps": ("STRING", {"default": "51°30'00\"N 0°10'00\"E"}),
                 "width": ("INT", {"default": 1280, "min": 64, "max": 2048}),
             }
         }
 
-    def to_image(self, model="COMFYUI X1 ULTRA", date=None, brand="leica",
+    def to_image(self, model="COMFYUI ULTRA", date=None, brand="liuqianhong",
                 device="23mm f/1.0 1/320 ISO1495", gps="51°30'00\"N 0°10'00\"E",
                 width=1280):
         if date is None:
@@ -63,7 +66,7 @@ class CameraWatermarkNode(BaseNode):
             'gps': gps,
             'width': width,
             'height': 512,
-            'svg_uri': self.svg_data_uris.get(brand, '')
+            'brand_uri': self.brand_data_uris.get(brand, '')
         }
         
         template = self.jinja_env.get_template('camera_watermark_template.html')
